77eaf15e7a45ee9c379595af3e64a30d6c83b3be
Antriksh <antrikshmanwadkar33@gmail.com>
2025-01-25 23:29:09 +0530

    Add files via upload

---
+++ Barcode Generator!!😛/script.js
@@ -0,0 +1,22 @@
+
+function generateBarcode() 
+{
+    const text = document.getElementById("barcodeText").value;
+    const barcodeElement = document.getElementById("barcode");
+
+    if (text) 
+        {
+        JsBarcode(barcodeElement, text, 
+        {
+            format: "CODE128", 
+            lineColor: "#000",
+            width: 2,
+            height: 100,
+            displayValue: true,
+        });
+    } 
+    else
+    {
+        alert("Please enter text or a number for the barcode");
+    }
+}
